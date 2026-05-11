9982f6b3245f7f0c8aec73ea6b53c65d28604448
FPV-agency <del81ne@gmail.com>
2026-05-11 22:06:06 +0300

    Fix: used relative paths with dot-slash base

--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,21 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-  // 1. Сначала загружаем переменные окружения
   const env = loadEnv(mode, '.', '');
 
-  // 2. Возвращаем объект конфигурации
   return {
-    base: '/', // ТЕПЕРЬ ОНО НА СВОЕМ МЕСТЕ
+    base: './',
     plugins: [react(), tailwindcss()],
-                            define: {
-                              'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-                            },
-                            resolve: {
-                              alias: {
-                                '@': path.resolve(__dirname, './src'), // Обычно алиас ведет в src
-                              },
-                            },
-                            server: {
-                              // HMR is disabled in AI Studio via DISABLE_HMR env var.
-                              hmr: process.env.DISABLE_HMR !== 'true',
-                            },
+    define: {
+    'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+    },
+    resolve: {
+    alias: {
+    '@': path.resolve(__dirname, './src'), // Обычно алиас ведет в src
+    },
+    },
+    server: {
+    hmr: process.env.DISABLE_HMR !== 'true',
+    },
   };
 });
